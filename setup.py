88510e45b2135baf8513f442b6098e88868ea3ed
unknown <kdykes@kdykes-22302s.nrel.gov>
2014-09-25 10:25:15 -0600

    updating setup for windows options

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import setuptools
 from numpy.distutils.core import setup, Extension
 from os.path import join
-
+import platform
 
 path = 'src'
 src = ['Poly.cpp', 'myMath.cpp', 'BeamFEA.cpp', 'Beam.cpp', 'pyBEAM.cpp']
@@ -20,17 +20,28 @@ src[4] = join(path, 'pyBEAM', src[4])
 # f.write('exclude ' + join(path, 'pBEAM', 'main.cpp') + '\n')
 # f.close()
 
-setup(
-    name='pBEAM',
-    version='0.1.0',
-    description='Polynomial Beam Element Analysis Module. Finite element analysis for beam-like structures.',
-    author='S. Andrew Ning',
-    author_email='andrew.ning@nrel.gov',
-    # install_requires=['numpy', 'scipy'],
-    license='Apache License, Version 2.0',
-    ext_modules=[Extension('_pBEAM', sources=src, extra_compile_args=['-O2'],
-                           include_dirs=[join(path, 'pBEAM'), 'C:/boost_1_55_0'],
-                           library_dirs=['C:/boost_1_55_0/stage/lib', 'C:/lapack'],
-                           libraries=['boost_python-mgw46-mt-1_55', 'lapack'])]
-
-)
+if platform.system() == 'Windows':
+    setup(
+        name='pBEAM',
+        version='0.1.0',
+        description='Polynomial Beam Element Analysis Module. Finite element analysis for beam-like structures.',
+        author='S. Andrew Ning',
+        author_email='andrew.ning@nrel.gov',
+        # install_requires=['numpy', 'scipy'],
+        license='Apache License, Version 2.0',
+        # Windows
+        ext_modules=[Extension('_pBEAM', sources=src, extra_compile_args=['-O2'],
+                               include_dirs=[join(path, 'pBEAM'), 'C:/boost_1_55_0'],
+                               library_dirs=['C:/boost_1_55_0/stage/lib', 'C:/lapack'],
+                               libraries=['boost_python-mgw46-mt-1_55', 'lapack'])])
+else:
+    setup(
+        name='pBEAM',
+        version='0.1.0',
+        description='Polynomial Beam Element Analysis Module. Finite element analysis for beam-like structures.',
+        author='S. Andrew Ning',
+        author_email='andrew.ning@nrel.gov',
+        # install_requires=['numpy', 'scipy'],
+        license='Apache License, Version 2.0',
+        # OS X, Linux
+        ext_modules=[Extension('_pBEAM', sources=src, extra_compile_args=['-O2'])])
