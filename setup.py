0b9e703eb79e3f969c724fa3469e4ff49b2c62a0
Evan Gaertner <evan.gaertner@gmail.com>
2018-06-19 16:01:52 -0600

    Updated setup.py for Windows

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 import setuptools
 from numpy.distutils.core import setup, Extension
-import os
+import os, platform
 
 path = 'src'
 src = ['Poly.cpp', 'myMath.cpp', 'BeamFEA.cpp', 'Beam.cpp', 'pyBEAM.cpp']
@@ -13,15 +13,29 @@ for i in range(4):
 src[4] = os.path.join(path, 'pyBEAM', src[4])
 
 
-setup(
-    name='pBEAM',
-    version='0.2.0',
-    description='Polynomial Beam Element Analysis Module. Finite element analysis for beam-like structures.',
-    author='S. Andrew Ning and Garrett E. Barter',
-    author_email='garrett.barter@nrel.gov',
-    license=['Apache License, Version 2.0','Mozilla Public License (MPL) version 2.0',
-             'Boost Software License 1.0','pybind11 license'],
-    ext_modules=[Extension('_pBEAM', sources=src, extra_compile_args=['-fPIC -std=c++11'],
-                           include_dirs=[os.path.join(path, 'pBEAM'), os.path.join(path, 'include')])],
-    zip_safe=False
-)
+if platform.system() == 'Windows':
+    setup(
+        name='pBEAM',
+        version='0.2.0',
+        description='Polynomial Beam Element Analysis Module. Finite element analysis for beam-like structures.',
+        author='S. Andrew Ning and Garrett E. Barter',
+        author_email='garrett.barter@nrel.gov',
+        license=['Apache License, Version 2.0','Mozilla Public License (MPL) version 2.0',
+                 'Boost Software License 1.0','pybind11 license'],
+        ext_modules=[Extension('_pBEAM', sources=src, extra_compile_args=['-std=gnu11','-fPIC'],
+                                   include_dirs=[os.path.join(path, 'pBEAM'), os.path.join(path, 'include')])],
+        zip_safe=False
+    )
+else:
+    setup(
+        name='pBEAM',
+        version='0.2.0',
+        description='Polynomial Beam Element Analysis Module. Finite element analysis for beam-like structures.',
+        author='S. Andrew Ning and Garrett E. Barter',
+        author_email='garrett.barter@nrel.gov',
+        license=['Apache License, Version 2.0','Mozilla Public License (MPL) version 2.0',
+                 'Boost Software License 1.0','pybind11 license'],
+        ext_modules=[Extension('_pBEAM', sources=src, extra_compile_args=['-std=c++11','-fPIC'],
+                                   include_dirs=[os.path.join(path, 'pBEAM'), os.path.join(path, 'include')])],
+        zip_safe=False
+    )
